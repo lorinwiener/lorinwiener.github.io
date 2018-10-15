b6ad071d9bb9716a9e6043a8de4c05e34a64fbbe
lorinwiener <lorin@lorinwiener.com>
2018-10-15 12:37:18 -0700

    update

--- staging/performance-cookies-script.js
+++ staging/performance-cookies-script.js
@@ -1,9 +1,1 @@
-<html>
-  <head>
-    <script type="text/plain" class="optanon-category-2">
-    document.cookie = "Adobe Analytics=Homepage Viewed";
-    </script>
-  </head>
-  <body bgcolor="FFFFFF">
-  </body>
-</html>
+document.cookie = "Adobe Analytics=Homepage Viewed";
